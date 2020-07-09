e1b8f3257580ba772b982f780f885ab7436b0b56
maxidiflumeri <maxidiflumeri@gmail.com>
2020-07-08 21:11:01 -0300

    Estados completo

--- public/cliente/src/components/Estados.vue
+++ public/cliente/src/components/Estados.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="container-fluid mt-3">    
+  <div class="container-fluid mt-3">        
+    <!-- -------------------------------------------------------------------------------------------------------------------- -->    
+    <!------------------------- Creacion de la tabla que muestra los resultados ------------------------------------------------>
+    <!-- -------------------------------------------------------------------------------------------------------------------- -->
     <md-table class="color" v-model="buscados" md-sort="name" md-sort-order="asc" md-card md-fixed-header @md-selected="onSelect">
       <md-table-toolbar>
         <div class="md-toolbar-section-start">
@@ -20,14 +23,20 @@
         <md-table-cell  md-label="Descripcion" md-sort-by="DESCRIPCION">{{ item.DESCRIPCION | primeraMayuscula}}</md-table-cell>
       </md-table-row>      
     </md-table>
+    <!-- -------------------------------------------------------------------------------------------------------------------- -->    
+    <!-------------------------------- Botones de creacion y actualizacion ----------------------------------------------------->
+    <!-- -------------------------------------------------------------------------------------------------------------------- -->
     <div class="mt-3 d-flex justify-content-center">
-      <md-button class="md-fab bg-success md-mini" @click="nuevoEstado()">
+      <md-button class="md-fab bg-success md-mini" @click="getEstados()">
         <md-icon>refresh</md-icon>
       </md-button>
-      <md-button class="md-fab md-primary md-mini" @click="nuevoEstado()">
+      <md-button class="md-fab md-primary md-mini" @click="mostrarNuevo()">
         <md-icon>add</md-icon>
       </md-button>
     </div> 
+    <!-- -------------------------------------------------------------------------------------------------------------------- -->    
+    <!-------------------------------- Card que muestra el detalle del elemento ------------------------------------------------>
+    <!-- -------------------------------------------------------------------------------------------------------------------- -->
     <div class="mt-3 d-flex justify-content-center">     
       <form v-if="estaSeleccionado" class="md-layout">
         <md-card :class="claseCard">
@@ -52,15 +61,15 @@
                   </div>
                 </div>
                 <div v-if="!estaEditando" class="row d-flex justify-content-center">
-                  <md-button class="md-fab md-mini bg-warning" @click="editar()">
+                  <md-button class="md-fab md-mini bg-warning" @click="habilitarEdicion()">
                     <md-icon>edit</md-icon>                    
                   </md-button>
-                  <md-button class="md-fab md-mini md-plain" @click="ventanaModal()" >
+                  <md-button class="md-fab md-mini md-plain" @click="activo = true" >
                     <md-icon>delete</md-icon>                    
                   </md-button>
                 </div>
                 <div v-else class="row d-flex justify-content-center">
-                  <md-button class="md-fab md-mini md-primary" @click="modificarEdicion(seleccionadoTemp)">
+                  <md-button class="md-fab md-mini md-primary" @click="confirmarEdicion(seleccionadoTemp)">
                     <md-icon>check</md-icon>                    
                   </md-button>
                   <md-button class="md-fab md-mini md-plain" @click="cancelarEdicion()">
@@ -73,14 +82,61 @@
         </md-card>
       </form>
     </div>
+    <!-- -------------------------------------------------------------------------------------------------------------------- -->    
+    <!---------------------------- Modal de confirmacion para eliminar elemento ------------------------------------------------>
+    <!-- -------------------------------------------------------------------------------------------------------------------- -->
     <md-dialog-confirm
       :md-active.sync="activo"
       md-title="Atencion"
-      md-content="¿Esta Seguro que desea eliminar el Estado?"
-      md-confirm-text="Aceptar"
+      md-content="¿Esta seguro que desea eliminar el registro?"
       md-cancel-text="Cancelar"
-      @md-cancel="onCancel"
-      @md-confirm="onConfirm(seleccionadoTemp)" />
+      md-confirm-text="Aceptar"
+      @md-cancel="activo = false"
+      @md-confirm="confirmarEliminar(seleccionadoTemp)" />
+    <!-- -------------------------------------------------------------------------------------------------------------------- -->    
+    <!----------------------------- Modal de mensaje de error al crear, Editar o eliminar elemento ------------------------------------------------>
+    <!-- -------------------------------------------------------------------------------------------------------------------- -->
+    <md-dialog-alert
+      :md-active.sync="hayError"
+      md-title="Error"
+      :md-content="mensajeError"
+      md-confirm-text="Ok" />
+    <!-- -------------------------------------------------------------------------------------------------------------------- -->    
+    <!----------------------------- Modal de formulario de creacion de elemento ------------------------------------------------>
+    <!-- -------------------------------------------------------------------------------------------------------------------- -->
+    <md-dialog :md-active.sync="estaCreando">      
+      <md-dialog-title>Nuevo registro</md-dialog-title>
+      <vue-form :state="formState" @submit.prevent="confirmarAgregar()">
+        <md-card class="md-layout-item md-size-100 md-small-size-100">
+          <md-card-content>
+            <div class="md-layout md-gutter">
+              <div class="container">
+                <md-field>
+                  <validate tag="div">
+                    <label>Id Estado</label>
+                    <md-input maxlength="1" name="id_estado" id="id_estado" v-model="formData.id_estado" required/>      
+                  </validate>
+                </md-field>
+                <md-field>
+                  <validate tag="div">
+                    <label>Descripcion</label>
+                    <md-input maxlength="30" name="descripcion" id="descripcion" v-model="formData.descripcion" required/>
+                  </validate>
+                </md-field>
+              </div>
+            </div>
+          </md-card-content>
+        </md-card>
+        <md-dialog-actions>
+          <md-button type="submit" class="md-raised md-primary" :disabled="formState.$invalid">Agregar</md-button>
+          <md-button class="md-primary" @click="estaCreando = false">Cancelar</md-button>
+        </md-dialog-actions>
+      </vue-form>
+
+
+
+    </md-dialog>   
+
   </div>
 
 
@@ -126,11 +182,32 @@
         claseCard: 'md-layout-item md-size-100 md-small-size-100 color',
         estaEditando: false,
         seleccionadoTemp: {},
-        activo: false     
+        activo: false,
+        estaCreando: false,
+        formState: {},
+        formData: this.getDatosIniciales(),
+        hayError: false,
+        mensajeError: ''          
 
       }
     },
     methods: {
+
+      // activa modal de creacion de elemento
+      mostrarNuevo(){
+        this.estaCreando = true
+        this.formData = this.getDatosIniciales()
+      },
+
+      // incializa datos del objeto de creacion de elemento
+      getDatosIniciales(){
+        return {
+          id_estado: '',
+          descripcion: ''
+        }
+      },
+
+      // metodo que trae todos los elementos
       getEstados() {
         this.axios.get(url.url + url.urlEstados, {
           headers:
@@ -147,12 +224,13 @@
           console.log('ERROR GET HTTP', error)
         })
       },
+
+      // buscador de elemento en la tabla
       buscarEnTabla () {
         this.buscados = buscarPorNombre(this.estados, this.busqueda)
       },
-      nuevoEstado () {
-        window.alert('Noop')
-      },
+
+      // seleccion de un elemento de la tabla que muestra el card con el detalle del elemento
       onSelect (item) {
         if(item){
           this.seleccionadoTemp = {
@@ -170,16 +248,17 @@
           this.estaEditando = false              
             
         }    
-
         console.log(this.seleccionado) 
          
       },
 
-      editar(){        
+      // habilita el card para la edición del elemento
+      habilitarEdicion(){        
         this.claseCard = 'md-layout-item md-size-100 md-small-size-100' 
         this.estaEditando = true
       },
-
+      
+      // cancela la edición del elemento
       cancelarEdicion(){
         this.estaEditando = false
         this.claseCard = 'md-layout-item md-size-100 md-small-size-100 color' 
@@ -187,15 +266,20 @@
         this.estaSeleccionado = false      
       },
 
-      modificarEdicion(estadoPut){
-        console.log("EstadoPut: " + estadoPut.id_estado + " " + estadoPut.descripcion)
+      // metodo que modifica el elemento
+      confirmarEdicion(estadoPut){        
         this.axios.put(url.url + url.urlEstados + '/'+ this.seleccionado.ID_ESTADO, estadoPut, {
           headers:
             {'Authorization': `Bearer ${this.$store.state.token.substr(1, this.$store.state.token.length-2)}`}          
           })
           .then( res => {  
-            console.log(res.data)    
-            this.getEstados()                      
+            if(res.data.estado == 200){
+              this.getEstados()
+            }else{
+              console.log(res.data)
+              this.hayError = true
+              this.mensajeError = res.data.mensaje || 'No se pudo realizar la operación'
+            }      
         })
         .catch(error => {
           console.log('ERROR GET HTTP', error)
@@ -204,32 +288,48 @@
         this.claseCard = 'md-layout-item md-size-100 md-small-size-100 color' 
       },
 
-      eliminarEstado(estadoDel){
+       // metodo que agrega el elemento
+      confirmarAgregar(){        
+        this.axios.post(url.url + url.urlEstados , this.formData, {
+          headers:
+            {'Authorization': `Bearer ${this.$store.state.token.substr(1, this.$store.state.token.length-2)}`}          
+          })
+          .then( res => { 
+            if(res.data.estado == 200){
+              this.getEstados()
+            }else{
+              this.hayError = true
+              this.mensajeError = res.data.mensaje || 'No se pudo realizar la operación'
+            }   
+                                  
+        })
+        .catch(error => {
+          console.log('ERROR GET HTTP', error)
+        })
+        this.estaCreando = false
+
+      },
+
+      // metodo que elimina el elemento
+      confirmarEliminar(estadoDel){
         this.axios.delete(url.url + url.urlEstados + '/'+ estadoDel.id_estado, {
           headers:
             {'Authorization': `Bearer ${this.$store.state.token.substr(1, this.$store.state.token.length-2)}`}          
           })
           .then( res => {  
-            console.log(res.data)    
-            this.getEstados()                      
+            if(res.data.estado == 200){
+              this.getEstados()
+            }else{
+              console.log(res.data)
+              this.hayError = true
+              this.mensajeError = res.data.mensaje || 'No se pudo realizar la operación'
+            }                      
         })
         .catch(error => {
           console.log('ERROR GET HTTP', error)
         })
 
       },
-
-      onConfirm (estadoDel) {
-        this.eliminarEstado(estadoDel)        
-      },
-      onCancel () {
-        this.activo = false
-      },
-
-      ventanaModal(){
-        this.activo = true
-      }
-
 
     },
     computed: {
